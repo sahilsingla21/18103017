84be2752acbd17cc5d2d66162ac4efb6aed43537
sahilsingla21 <63259776+sahilsingla21@users.noreply.github.com>
2020-11-30 13:05:54 +0530

    Update CountingSort.java

--- Assignment2/CountingSort.java
+++ Assignment2/CountingSort.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Assignment2;
 
 import java.util.Arrays;
-
-/**
- *
- * @author Suhel Naryal
- */
 public class CountingSort {
     
     public void sort(int[] arr){
@@ -28,11 +19,7 @@ public class CountingSort {
         
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         
         int[] arr = {2,4,1,5,7,2,4,6,1,0};
         (new CountingSort()).sort(arr);
