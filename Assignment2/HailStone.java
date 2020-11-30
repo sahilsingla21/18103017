2dcd1a3778be189bd49cecdf811ebcd1794d64c2
sahilsingla21 <63259776+sahilsingla21@users.noreply.github.com>
2020-11-30 13:06:31 +0530

    Update HailStone.java

--- Assignment2/HailStone.java
+++ Assignment2/HailStone.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Assignment2;
 
 import java.util.ArrayList;
-
-/**
- *
- * @author Suhel Naryal
- */
 public class HailStone {
     
     public Integer[] hailstone(int n){
@@ -25,12 +16,8 @@ public class HailStone {
         
         return list.toArray(new Integer[list.size()]);
     }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
